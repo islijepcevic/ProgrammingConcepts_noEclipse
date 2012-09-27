4e5944adaf449bd65c5ceec690b90d49bdb4fb0e
islijepcevic <islijepcevic@gmail.com>
2012-09-27 22:14:21 +0200

    about to finish first chapter

--- README.md
+++ README.md
@@ -5,15 +5,32 @@ Programming Concepts in Scientific Computing
 
 
 Command line instructions:
+--------------------------
 
 1) first time:
+
 $ cd ./build/opt
+
 $ ./do-configure.sh
 
 2) for every new source:
-modify CMakeLists.txt in belonging directory: add line
+modify CMakeLists.txt in belonging directory: add line;
 add_executable(executable_name source_name.cpp)
 
 from root of repo run:
+
 $ make all
 
+
+Git pulling
+-----------
+
+commit (last time)
+
+git pull --rebase origin_deparis
+
+get rid of conflicts
+
+save, add to index, commit
+
+push
--- chapter-01/exercise-template.cpp
+++ chapter-01/exercise-template.cpp
@@ -1,11 +1,11 @@
 /*
- * exercise-template.cpp <--- The name of the source file goes here
+ * exercise_1_2.cpp
  *
- * <--- Description of the program goes here.
+ * writes product of two input integers
  *
- * Created on: Jul 26, 2012 <--- Fill in the date here
- * 	   Author: Radu Popescu <radu.popescu@epfl.ch> <--- Fill in your name
- * 	   																					   (and e-mail) here
+ * Created on: Sept 24, 2012
+ * 	   Author: Ivan Slijepcevic <ivan.slijepcevic@epfl.ch>
+ *
  */
 
 /*
@@ -13,11 +13,7 @@
  */
 #include <iostream>
 
-int main(int argc, char* argv[])
-{
-	/*
-	 * The body of the program goes here
-	 */
+int main(int argc, char* argv[]) {
     return 0;
 }
 

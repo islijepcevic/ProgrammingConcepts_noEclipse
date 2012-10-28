426554583a9b948c68aece71e95bdfb0a0eb81a2
Radu Popescu <radu.popescu@epfl.ch>
2012-10-28 12:50:58 +0100

    Quiz03 solution

--- quiz-solutions/quiz03/NewtonSolver.hpp
+++ quiz-solutions/quiz03/NewtonSolver.hpp
@@ -23,4 +23,45 @@ int NewtonSolver(dataType& x0,
 		         const int maxIter);
 
 
+template<typename dataType>
+int NewtonSolver(dataType& solution,
+		         dataType (*function) (const dataType&),
+		         dataType (*inverseJacobian) (const dataType&, const dataType&),
+		         const double tol,
+		         const int maxIter)
+{
+	dataType delta;
+	dataType f_value;
+	double error;
+	int numIter = 0;
+
+	f_value = function(solution);
+	error = f_value.norm();
+
+	std::cout << "x[" << numIter << "] = " << solution
+			  << " \t f(x) = " << f_value << std::endl;
+
+	while (error > tol && numIter < maxIter)
+	{
+		delta = inverseJacobian(solution, f_value);
+		solution = solution - delta;
+		++numIter;
+
+		f_value = function(solution);
+		error = f_value.norm();
+
+		std::cout << "x[" << numIter << "] = " << solution
+				  << " \t f(x) = " << f_value << std::endl;
+	}
+
+	if ( error > tol )
+	{
+		std::cout << "Max number of iterations reached without convergence."
+				  << std::endl;
+		return -numIter;
+	}
+
+	return numIter;
+}
+
 #endif /* NEWTONSOLVER_HPP_ */

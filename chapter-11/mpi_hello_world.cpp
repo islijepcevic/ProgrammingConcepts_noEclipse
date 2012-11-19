a39f68b53f9608338cbbf7b7274fd44c9401735d
islijepcevic <islijepcevic@gmail.com>
2012-11-19 08:54:21 +0100

    test mpi

---
+++ chapter-11/mpi_hello_world.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <mpi.h>
+
+int main(int argc, char* argv[]) {
+
+    MPI::Init(argc, argv);
+
+    int num_procs = MPI::COMM_WORLD.Get_size();
+    int rank = MPI::COMM_WORLD.Get_rank();
+    std::cout << "Hello world from process " << rank
+        << " of " << num_procs << "\n";
+
+    MPI::Finalize();
+    
+    return 0;
+}
